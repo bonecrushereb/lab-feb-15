eb29786b7aebc0c1a40b7e20f0839d96dc9ba2db
bonecrushereb <bonecrushereb@gmail.com>
2016-02-15 10:34:13 -0800

    added problem3

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+
 function sum(sum1, sum2){
   var sum = sum1 + sum2;
-  console.log('The sum of ' + sum1 + ' + ' + sum2 + ' = ' + sum + '.');
+  //console.log('The sum of ' + sum1 + ' + ' + sum2 + ' = ' + sum + '.');
   return sum;
 }
 
-function multi(mult1,multi2){
+function multiply(multi1,multi2){
   var multi = multi1 * multi2;
-  console.log('The product of ' + mutli1 + ' and ' + multi2 + ' is ' + multi);
+  //console.log('The product of ' + mutli1 + ' and ' + multi2 + ' is ' + multi);
   return multi;
+}
+
+function sumAndMultiply(num1, num2, num3){
+  var sumof2 = sum(num1,num2);
+  var totalSum = sum(sumof2, num3);
+
+  var multiplyof2 = multiply(num1,num2);
+  var totalMultiply = multiply(multiplyof2, num3);
+
+  console.log(num1 + ' and ' + num2 + ' and ' + num3 + ' sum to ' + totalSum + '.');
+
+  console.log('The numbers ' + num1 + ' and ' + num2 + ' and ' + num3 + ' have a product ' + ' of ' + totalMultiply + '.');
+
+  return [totalSum, totalMultiply];
+
+
 }
