b8c6b2581f24f98cf0a5904abb82e79387b919c1
bonecrushereb <bonecrushereb@gmail.com>
2016-02-15 08:59:44 -0800

    added problem 2 to app.js

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ function sum(num1, num2){
   console.log('The sum of ' + num1 + ' + ' + num2 + ' = ' + sum + '.');
   return sum;
 }
+
+function multi(num1,num2){
+  var multi = num1 * num2;
+  console.log('The product of ' + num1 + ' and ' + num2 + ' is ' + multi);
+  return multi;
+}
